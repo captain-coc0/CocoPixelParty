d760060f67f59db46a29a5ef7257998e1e93d739
captain-c0c0 <exoticblox@gmail.com>
2026-07-14 02:43:15 -0400

    vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       "/api": "http://backend:5000",
       "/socket.io": {
